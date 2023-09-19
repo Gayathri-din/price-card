78d51690bf3dc0ad74dda48a96ceb87798345fd5
Gayathri-din <gayu.abgl@gmail.com>
2023-09-19 15:14:22 +0530

    price card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+const App = () => {
+  return(
+    <div class="container">
+      <div class="box">
+        
+       
+        <div class="box1">
+          <p >FREE</p>
+
+          <center>$0/ MONTH</center>
+          <ul>
+            <li>single User</li>
+            <li>50GB Storgae</li>
+            <li>Unlimited Public Project</li>
+            <li>Community Access</li>
+          </ul>
+          <ul >
+            <li class="blur" >Unlimited Private Project</li>
+            <li class="blur" >dedicated Phone support</li>
+            <li class="blur">Free subdomain</li>
+            <li class="blur">Monthly Status Report</li>
+          </ul>
+        <button class="btn1">BUTTON</button>
+        </div>
+
+
+
+        <div class="box2">
+          <p >PLUS</p>
+          <center>$9/ MONTH</center>
+          <ul>
+            <li>5 User</li>
+            <li>50GB Storage</li>
+            <li>Unlimited Public Projects</li>
+            <li>Community Acess</li>
+          </ul>
+          <ul >
+            <li >Unlimited Private Projects</li>
+            <li >dedicated Phone support</li>
+            <li >Free subdomain</li>
+            <li class="blur">Monthly Status Report</li>
+          </ul>
+          <button class="btn2">BUTTON</button>
+        </div>
+
+
+
+        <div class="box3">
+          <p >PRO</p>
+          <center>$49/ MONTH</center>
+          <ul>
+            <li>Unlimited User</li>
+            <li>50GB Storage</li>
+            <li>Unlimited Public Projects</li>
+            <li>Community Acess</li>
+          </ul>
+          <ul >
+            <li >Unlimited Private Projects</li>
+            <li >Dedicated Phone support</li>
+            <li >Free subdomain</li>
+            <li>Monthly Status Report</li>
+          </ul>
+          <button class="btn3" >BUTTON</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
-export default App
+export default App;
